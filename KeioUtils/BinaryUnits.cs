642379f00db5c94060c48fff918093ec82d501e7
kuro68k <mojo@world3.net>
2017-01-19 14:16:07 +0000

    Binary unit parsing

--- KeioUtils/BinaryUnits.cs
+++ KeioUtils/BinaryUnits.cs
@@ -37,5 +37,54 @@ namespace Keio.Utils
 			return v.ToString(format);
 		}
 
+		public static bool ParseBinaryUnitNumber(string s, out double d)
+		{
+			d = 0;
+
+			double multiplier = 1;
+
+			// find suffix
+			bool number_found = false;
+			string suffix = null;
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (char.IsDigit(s[i]) || (s[i] == '.'))
+				{
+					number_found = true;
+				}
+				else
+				{
+					suffix = s.Substring(i).Trim();
+					s = s.Substring(0, i);
+				}
+			}
+			if (!number_found)	// no numeric part
+				return false;
+
+			if ((suffix != null) && (suffix.Length > 1))	// there is a suffix but it is too long
+				return false;
+
+			if (suffix != null)
+			{
+				int idx = 0;
+				for (int exp = 1; exp <= 8; exp ++)
+				{
+					if (suffix == Prefixes[idx])
+					{
+						multiplier = Math.Pow(1024, (double)exp);
+						break;
+					}
+					idx++;
+				}
+			}
+
+			if (double.TryParse(s, out d))
+			{
+				d *= multiplier;
+				return true;
+			}
+
+			return false;
+		}
 	}
 }
